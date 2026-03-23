c4821cc901c94ebeef143ffea796623b54ef8fc3
hal <shinycelebi@protonmail.com>
2026-03-23 16:58:30 -0400

    Implement basic button functionality

--- script.js
+++ script.js
@@ -18,134 +18,51 @@ game screen:
     - display play again button (refreshes game)
 */
 
-/* ---------------- TESTING ------------------ */
-// playGame();
+/* ----------------- VARIABLES ------------------ */
 
-/* ---------- CREATE DOM ELEMENTS ------------ */
-const gameWindow = document.querySelector('#gameWindow');
+const gameWindow = document.querySelector("#gameWindow");
 
-const moveSelectBox = document.createElement('div');
-moveSelectBox.setAttribute("id", "move");
+const rockBtn = document.querySelector("#rockBtn");
+const paperBtn = document.querySelector("#paperBtn");
+const scissorsBtn = document.querySelector("#scissorsBtn");
 
-const moveSelectText = document.createElement('p');
-moveSelectText.textContent = "Make your move:";
+let humanChoice = "";
+let hasChosenMove = false;
 
-const buttonsBox = document.createElement('div');
-buttonsBox.setAttribute("id", "buttons");
-
-const rockBtn = document.createElement('button');
-rockBtn.textContent = "ROCK";
-rockBtn.setAttribute("id", "rockBtn");
-const paperBtn = document.createElement('button');
-paperBtn.textContent = "PAPER";
-paperBtn.setAttribute("id", "paperBtn");
-const scissorsBtn = document.createElement('button');
-scissorsBtn.textContent = "SCISSORS";
-scissorsBtn.setAttribute("id", "scissorsBtn");
-
-const rockIcon = document.createElement('img');
-rockIcon.setAttribute("src", "resources/rock.png");
-rockIcon.setAttribute("alt", "");
-rockIcon.setAttribute("class", "rockIcon");
-const paperIcon = document.createElement('img');
-paperIcon.setAttribute("src", "resources/paper.png");
-paperIcon.setAttribute("alt", "");
-paperIcon.setAttribute("class", "paperIcon");
-const scissorsIcon = document.createElement('img');
-scissorsIcon.setAttribute("src", "resources/scissors.png");
-scissorsIcon.setAttribute("alt", "");
-scissorsIcon.setAttribute("class", "scissorsIcon");
-
-const resultsBox = document.createElement('div');
-resultsBox.setAttribute("id", "results");
-
-const playerChoicePara = document.createElement('p');
-const playerChoiceSpan = document.createElement('span');
-const playerChoiceIcon = document.createElement('img');
-playerChoiceIcon.setAttribute("alt", "");
-
-const computerChoicePara = document.createElement('p');
-const computerChoiceSpan = document.createElement('span');
-const computerChoiceIcon = document.createElement('img');
-computerChoiceIcon.setAttribute("alt", "");
-
-const scoreBox = document.createElement('div');
-scoreBox.setAttribute("id", "score");
-const playerScoreText = document.createElement('p');
-const computerScoreText = document.createElement('p');
-
-const winnerText = document.createElement('p');
-const retryBtn = document.createElement('button');
-retryBtn.setAttribute("id", "retry");
-retryBtn.textContent = "RETRY";
-const retryImg = document.createElement('img');
-retryImg.setAttribute("src", "resources/retry.png");
-retryImg.setAttribute("alt", "");
-retryImg.setAttribute("id", "retryImg");
-
-/* ---------- DOM PARENTING TEST ZONE ------------ */
-
-// choice buttons
-rockBtn.appendChild(rockIcon);
-paperBtn.appendChild(paperIcon);
-scissorsBtn.appendChild(scissorsIcon);
-
-buttonsBox.appendChild(rockBtn);
-buttonsBox.appendChild(paperBtn);
-buttonsBox.appendChild(scissorsBtn);
-
-moveSelectBox.appendChild(moveSelectText);
-moveSelectBox.appendChild(buttonsBox);
-
-gameWindow.appendChild(moveSelectBox);
-
-// results
-playerChoicePara.textContent = "Your choice: ";
-computerChoicePara.textContent = "Computer's choice: ";
-
-resultsBox.appendChild(playerChoicePara);
-resultsBox.appendChild(computerChoicePara);
-
-gameWindow.appendChild(resultsBox);
-
-// score
-playerScoreText.textContent = "Your score: ";
-computerScoreText.textContent = "Computer's score: ";
-winnerText.textContent = "Winner: you!";
-scoreBox.appendChild(playerScoreText);
-scoreBox.appendChild(computerScoreText);
-scoreBox.appendChild(winnerText);
-
-retryBtn.appendChild(retryImg);
-scoreBox.appendChild(retryBtn);
-
-gameWindow.appendChild(scoreBox);
+let humanScore = 0;
+let computerScore = 0; 
+let round = 1;
 
 /* --------------- HANDLE CLICKS ---------------- */
 
-// commenting out all of this to just focus on the DOM for now
-/*
-
 gameWindow.addEventListener('click', (Event) => {
-  Event.preventDefault();
   let target = Event.target;
-  let choice = -1;
+
+  if (!hasChosenMove && humanScore < 5 && computerScore < 5) {
+    switch(target.id) {
+      case 'rockBtn':
+        humanChoice = "rock";
+        break;
+      case 'paperBtn':
+        humanChoice = "paper";
+        break;
+      case 'scissorsBtn':
+        humanChoice = "scissors";
+        break;
+    }
+    hasChosenMove = true;
+    console.log(humanChoice);
+    playRound(humanChoice);
+    }
 
   switch(target.id) {
-    case 'btnRock':
-      choice = 0;
+    case 'retryBtn':
+      // restart game
       break;
-    case 'btnPaper':
-      choice = 1;
-      break;
-    case 'btnScissors':
-      choice = 2;
-      break;
-  }
-  console.log(choice);
+    }
 });
 
-/* --------------- GAME LOGIC ---------------- */ /*
+/* --------------- GAME LOGIC ---------------- */
 function getComputerChoice() {
   let randomNumber = Math.floor((Math.random()*3));
   let choice = "";
@@ -160,48 +77,41 @@ function getComputerChoice() {
     case 2:
       choice = "scissors";
       break;
-    default:
-      console.log("Error: RPS choice outside expected range.");
   }
   return choice;
 }
 
-// assumes all inputs are valid; fine for button press
-function getHumanChoice() {
-  let choice = "";
-  choice = window.prompt("Rock, paper, scissors, shoot!");
-  choice = choice.toLowerCase();
-  return choice;
-}
+function playRound() {
+  let computerChoice = getComputerChoice();
 
-function playGame() {
-  let humanScore = 0;
-  let computerScore = 0; 
-  let winner = "";
-  let round = 1;
-  
-  // (loop goes here: loop until one score reaches 5)
-
-  function playRound() {
-    let humanChoice = getHumanChoice();
-    let computerChoice = getComputerChoice();
-
-    if (humanChoice === "rock" && computerChoice === "scissors" ||
-            humanChoice === "paper" && computerChoice === "rock" || 
-            humanChoice === "scissors" && computerChoice === "paper") {
-      console.log("Round " + round + ": " + "You win! Your " + humanChoice + " beats computer's " + computerChoice + ".");
-      humanScore++;
-    }
-    else if (humanChoice === "rock" && computerChoice === "paper" ||
-            humanChoice === "paper" && computerChoice === "scissors" || 
-            humanChoice === "scissors" && computerChoice === "rock") {
-      console.log("Round " + round + ": " + "You lose. Computer's " + computerChoice + " beats your " + humanChoice + ".");
-      computerScore++;
-    }
-    else {
-      console.log("Error deciding a winner.");
-    }
+  if (humanChoice == computerChoice) {
+    console.log("tie");
   }
-}
+  else if (humanChoice === "rock" && computerChoice === "scissors" ||
+           humanChoice === "paper" && computerChoice === "rock" || 
+           humanChoice === "scissors" && computerChoice === "paper") {
+    console.log("Round " + round + ": " + "You win! Your " + humanChoice + " beats computer's " + computerChoice + ".");
+    humanScore++;
+  }
+  else if (humanChoice === "rock" && computerChoice === "paper" ||
+           humanChoice === "paper" && computerChoice === "scissors" || 
+           humanChoice === "scissors" && computerChoice === "rock") {
+    console.log("Round " + round + ": " + "You lose. Computer's " + computerChoice + " beats your " + humanChoice + ".");
+    computerScore++;
+  }
+  else {
+    console.log("Error deciding a winner.");
+  }
 
-*/
+  hasChosenMove = false;
+  humanChoice = "";
+  round++;
+
+  if (humanScore == 5) {
+    console.log("you win!");
+  }
+  else if (computerScore == 5) {
+    console.log("computer wins!");
+  }
+
+  }
